bc82ab888e70b67ba2c1c5d584d4ed14288fa283
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2020-05-29 23:05:29 -0400

    Install nftables now.

--- server-firewall.sh
+++ server-firewall.sh
@@ -38,3 +38,61 @@ function dist-check() {
 
 # Check Operating System
 dist-check
+
+# Install
+function install-firewall() {
+    if [ "$DISTRO" == "debian" ]; then
+      apt-get update
+      apt-get install haveged fail2ban -y
+    fi
+    if [ "$DISTRO" == "ubuntu" ]; then
+      apt-get update
+      apt-get install haveged fail2ban -y
+    fi
+    if [ "$DISTRO" == "raspbian" ]; then
+      apt-get update
+      apt-get install haveged fail2ban -y
+    fi
+    if [ "$DISTRO" == "arch" ]; then
+      pacman -Syu
+      pacman -Syu --noconfirm haveged fail2ban
+    fi
+    if [ "$DISTRO" == "fedora" ]; then
+      dnf update -y
+      dnf install haveged fail2ban -y
+    fi
+    if [ "$DISTRO" == "centos" ]; then
+      yum update -y
+      yum install haveged fail2ban -y
+    fi
+    if [ "$DISTRO" == "rhel" ]; then
+      yum update -y
+      yum install haveged fail2ban -y
+    fi
+    if pgrep systemd-journal; then
+      systemctl enable fail2ban
+      systemctl restart fail2ban
+    else
+      service fail2ban enable
+      service fail2ban restart
+    fi
+}
+
+install-firewall
+
+function secure-ssh() {
+    if [ ! -f "/root/.ssh/authorized_keys" ]; then
+      sed -i 's|#PasswordAuthentication yes|PasswordAuthentication no|' /etc/ssh/sshd_config
+      sed -i 's|#PermitEmptyPasswords no|PermitEmptyPasswords no|' /etc/ssh/sshd_config
+      sed -i 's|AllowTcpForwarding yes|AllowTcpForwarding no|' /etc/ssh/sshd_config
+      sed -i 's|X11Forwarding yes|X11Forwarding no|' /etc/ssh/sshd_config
+      sed -i 's|#Port 22|Port 22|' /etc/ssh/sshd_config
+    fi
+    if pgrep systemd-journal; then
+      systemctl enable sshd
+      systemctl restart sshd
+    else
+      service ssh enable
+      service ssh restart
+    fi
+}
